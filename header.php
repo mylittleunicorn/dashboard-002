9a3a3a91b809b639ed81ceb0f9f769095a1d1868
nasir14 <nasir@smkn1rongga.sch.id>
2023-02-19 09:44:41 +0700

    menambahkan gambar

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SuperaseR - Dasboard 001</title>
+        
+        <!-- LINK BOOTSTRAP CSS -->
+        <!-- <link rel="stylesheet" href="dist/css/bootstrap.min.css"> -->
+
+        <!-- LINK BOOTSTRAP ICONS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+        <!-- LINK CSS -->
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- LINK GOOGLE FONTS -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    </head>
+    <body>
